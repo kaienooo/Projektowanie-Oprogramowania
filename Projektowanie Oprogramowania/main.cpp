f17793f48e290c3e00c74ed56071666235c9105d
kaienooo <152631200+kaienooo@users.noreply.github.com>
2024-12-12 18:50:21 +0100

    Revert "Dev TEST co to za przycisk"

--- Projektowanie Oprogramowania/main.cpp
+++ Projektowanie Oprogramowania/main.cpp
@@ -1,11 +1,4 @@
 #include <iostream>
-// Tutaj tylko importujemy klasy z plikow osobnych, i w mainie ma byc jak najmniej rzeczy
-// mysle ze funkcja main bedzie miala mniej niz 100 loc
-
-
-// Jak bedziemy trzymac strukture podobna do tego co pisze z tym problemem to nie powinnismy sobie wchodzic w parede i ladnie powinno sie commitowac
-// w sensie ze ja potrzebuje cos co wy robicie i to sie sprowadza do tego ze wpisuje #include "waszaklasa.h" i dzia³a lub nie dzia³a
-#include <string>
 
 int main()
 {
